e85f9299ed0b0474a5142c63302721ae9cc9e63f
Matias Piipari <matias.piipari@gmail.com>
2023-05-28 14:06:51 +0300

    Charm release script seems to be happy now

--- release/main.py
+++ release/main.py
@@ -1,91 +1,110 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
 import subprocess
 
+import yaml
+
 parser = argparse.ArgumentParser()
-parser.add_argument("--image", required=True)
-parser.add_argument("--charm", required=True)
-parser.add_argument("--resource-name", required=True)
 parser.add_argument("--charm-path", required=True)
 
 args = parser.parse_args()
 
-image_name, tag = args.image.rsplit(":", 1)
+charm_dir = os.path.dirname(args.charm_path)
+metadata_path = os.path.join(charm_dir, "metadata.yaml")
+charm_name = os.path.basename(args.charm_path).split("_")[0]
 
-pull_command = subprocess.run(["docker", "pull", args.image], check=True)
-if pull_command.returncode != 0:
-    exit(pull_command.returncode)
+with open(metadata_path, "r") as stream:
+    try:
+        data = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+        exit(1)
 
-digest_command = subprocess.run(
-    [
-        "docker",
-        "images",
-        "--digests",
-        "--no-trunc",
-        "--format",
-        "{{.Digest}}",
-        image_name,
-    ],
-    check=True,
-    capture_output=True,
-    text=True,
-)
-if digest_command.returncode != 0:
-    exit(digest_command.returncode)
-digest = digest_command.stdout.strip()
-print(f"Digest of image: {digest}")
+resources = data.get("resources", {})
+resource_revisions = []
 
-upload_resource_command = subprocess.run(
-    [
-        "charmcraft",
-        "upload-resource",
-        "--image",
-        digest,
-        args.charm,
-        args.resource_name,
-    ],
-    check=True,
-    capture_output=True,
-    text=True,
-)
-if upload_resource_command.returncode != 0:
-    exit(upload_resource_command.returncode)
+for resource_name, resource_data in resources.items():
+    if resource_data["type"] == "oci-image":
+        image = resource_data["upstream-source"]
 
-output_lines = upload_resource_command.stdout.splitlines()
-for line in output_lines:
-    if line.startswith("Revision"):
-        resource_revision = line.split()[1]
-        print(f"Revision of resource: {resource_revision}")
+        pull_command = subprocess.run(["docker", "pull", image], check=True)
+        if pull_command.returncode != 0:
+            exit(pull_command.returncode)
+
+        image_name, image_tag = image.rsplit(":", 1)
+
+        tag_digest_command = subprocess.run(
+            [
+                "docker",
+                "images",
+                "--no-trunc",
+                "--filter",
+                f"reference={image_name}",
+                "--format",
+                "{{.Tag}} {{.Digest}}",
+            ],
+            check=True,
+            capture_output=True,
+            text=True,
+        )
+
+        if tag_digest_command.returncode != 0:
+            exit(tag_digest_command.returncode)
+
+        output_lines = tag_digest_command.stdout.splitlines()
+        digest = None
+        for line in output_lines:
+            tag, line_digest = line.split()
+            if tag == image_tag:
+                digest = line_digest
+                break
+
+        if digest is None:
+            print(f"Could not find tag {image_tag} for image {image_name}")
+            exit(1)
+
+        upload_resource_command = subprocess.run(
+            [
+                "charmcraft",
+                "upload-resource",
+                "--image",
+                digest,
+                charm_name,
+                resource_name,
+            ],
+            check=True,
+            capture_output=True,
+            text=True,
+        )
+        if upload_resource_command.returncode != 0:
+            exit(upload_resource_command.returncode)
+
+        output_lines = upload_resource_command.stdout.splitlines()
+        for line in output_lines:
+            if line.startswith("Revision"):
+                resource_revision = line.split()[1]
+                print(f"Revision of resource {resource_name}: {resource_revision}")
+                resource_revisions.append(f"{resource_name}:{resource_revision}")
 
 upload_charm_command = subprocess.run(
-    ["charmcraft", "upload", args.charm_path], check=True
+    ["charmcraft", "upload", args.charm_path], check=True, capture_output=True, text=True
 )
 if upload_charm_command.returncode != 0:
-    exit(upload_charm_command.returncode)
-
-revisions_command = subprocess.run(
-    ["charmcraft", "revisions", args.charm], check=True, capture_output=True, text=True
-)
-if revisions_command.returncode != 0:
-    exit(revisions_command.returncode)
-
-output_lines = revisions_command.stdout.splitlines()
-for line in output_lines:
-    if line.startswith("1"):
-        charm_revision = line.split()[0]
-        print(f"Revision of charm: {charm_revision}")
+    output_lines = upload_charm_command.stderr.splitlines()
+    for line in output_lines:
+        if "Revision of the existing package is:" in line:
+            charm_revision = line.split(": ")[-1]
+else:
+    output_lines = upload_charm_command.stdout.splitlines()
+    for line in output_lines:
+        if "Revision" in line and "created" in line:
+            charm_revision = line.split()[1]
 
 release_command = subprocess.run(
-    [
-        "charmcraft",
-        "release",
-        args.charm,
-        "--revision",
-        charm_revision,
-        "--channel=beta",
-        f"--resource={args.resource_name}:{resource_revision}",
-    ],
+    ["charmcraft", "release", charm_name, "--revision", charm_revision, "--channel=beta"]
+    + [f"--resource={rr}" for rr in resource_revisions],
     check=True,
 )
 if release_command.returncode != 0:
